e2900c8038971c7db5c67489031702d4aae2c6f1
Emiliano Martin Gomez <gomez.emiliano.martin@gmail.com>
2022-08-27 21:24:17 -0300

    prueba

--- TrabajoPracticoDos/src/trabajopracticodos/TrabajoPracticoDos.java
+++ TrabajoPracticoDos/src/trabajopracticodos/TrabajoPracticoDos.java
@@ -14,7 +14,7 @@ public class TrabajoPracticoDos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HOla Mundos");// TODO code application logic here
     }
     
 }
